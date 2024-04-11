3a7125de14c8eeadd708a9428c3a94129cd1e8e7
Nurkhan <n.onerbayev@gmail.com>
2024-04-11 15:17:28 +0500

    update after lint again(

---
+++ homework/nurkhan_onerbayev/Homework_3/homework_three.py
@@ -0,0 +1,38 @@
+# 1
+a = 5
+b = 5
+print(a + b)
+a = 2
+b = 2
+print(a - b)
+a = 6
+b = 6
+print(a * b)
+
+# 2
+x = 5
+y = 5
+result = x - y / (1 + x * y)
+print(result)
+
+# 3
+a = 4
+b = 9
+
+average_arithmetic = (a + b) / 2
+
+average_geometric = (a * b) ** 0.5
+
+print(average_arithmetic)
+print(average_geometric)
+
+# 4
+a = 3
+b = 4
+
+c = (a**2 + b**2)**0.5
+
+s = 0.5 * a * b
+
+print(c)
+print(s)
