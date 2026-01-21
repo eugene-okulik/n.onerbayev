9911d1c6d2b049872be78ec9553e3d85807185e1
Nurkhan <n.onerbayev@gmail.com>
2026-01-21 23:07:22 +0500

    homework3 done

---
+++ homework/nurkhan_onerbayev/Homework3_2/homework3_2.py
@@ -0,0 +1,44 @@
+# Task 1
+a = 5
+b = 5
+print(a + b)
+
+
+a = 2
+b = 2
+print(a - b)
+
+
+a = 6
+b = 6
+print(a * b)
+
+# Task 2
+x = 5
+y = 5
+result = x - y / (1 + x * y)
+print(result)
+
+
+# Task 3
+a = 4
+b = 9
+
+average_arithmetic = (a + b) / 2
+
+average_geometric = (a * b) ** 0.5
+
+print(average_arithmetic)
+print(average_geometric)
+
+
+# Task 4
+a = 3
+b = 4
+
+c = (a ** 2 + b ** 2) ** 0.5
+
+s = 0.5 * a * b
+
+print(c)
+print(s)
