51c73993e44a08bd4bd33b9dcbadad93591115b9
Nurkhan <n.onerbayev@gmail.com>
2026-02-08 00:09:49 +0500

    homework7 done

---
+++ homework/nurkhan_onerbayev/homework_7/homework7_task3.py
@@ -0,0 +1,21 @@
+# Task3
+
+number1 = "результат операции: 42"
+
+number2 = "результат операции: 54"
+ 
+number3 = "результат работы программы: 209"
+
+number4 = "результат: 2"
+
+# Result1
+def rise_result(response: str):
+    result = response.split(':')
+    return(int(result[-1]) + 10)
+
+print(rise_result(number1))
+print(rise_result(number2))
+print(rise_result(number3))
+print(rise_result(number4))
+
+
