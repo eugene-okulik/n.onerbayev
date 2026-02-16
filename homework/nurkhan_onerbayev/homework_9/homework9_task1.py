a8fe8190694c22dc7a1c8235263f92713265a1b0
Nurkhan <n.onerbayev@gmail.com>
2026-02-16 14:17:35 +0500

    homework9 update month

--- homework/nurkhan_onerbayev/homework_9/homework9_task1.py
+++ homework/nurkhan_onerbayev/homework_9/homework9_task1.py
@@ -5,7 +5,11 @@ task_date = "Jan 15, 2023 - 12:05:33"
 
 python_date = datetime.strptime(task_date, '%b %d, %Y - %H:%M:%S')
 
-print(python_date.month)
+month_name = datetime.strftime(python_date, '%b')
+
+print(python_date)
+
+print(month_name)
 
 
 task_date2 = python_date.strftime('%d.%m.%Y, %H:%M')
